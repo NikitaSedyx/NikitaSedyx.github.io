353f5318fc761a255274c26a5c039bb06fcc02f1
Nikita Sedyh <nikita.sedyx.94@gmail.com>
2016-11-22 17:35:19 +0300

    modules - tr js

---
+++ task2/js/constants.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var newsApi = 'https://newsapi.org/v1/';
+var apiKey = 'b6fe1b1282f6405aae8087563e902c75';
+
+var categories = [{
+	name: 'all',
+	code: ''
+}, {
+	name: 'business',
+	code: 'business'
+}, {
+	name: 'entertainment',
+	code: 'entertainment'
+}, {
+	name: 'gaming',
+	code: 'gaming'
+}, {
+	name: 'general',
+	code: 'general'
+}, {
+	name: 'music',
+	code: 'music'
+}, {
+	name: 'sport',
+	code: 'sport'
+}, {
+	name: 'technology',
+	code: 'technology'
+}];
