edae7bb7900eb1596fbd0496a25cd4ed64ec2078
NikitaSedyx <nikita.sedyx.94@gmail.com>
2017-01-18 09:08:50 +0300

    hello world

--- task6/src/App/App.jsx
+++ task6/src/App/App.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
     return (
       <div className='app_container'>
         <Navigation></Navigation>
-        {this.props.children}
+        {this.props.children ? this.props.children : 'Hello World!'}
       </div>
     );
   }
